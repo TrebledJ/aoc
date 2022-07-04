3524f5c86ca81bb23fd6b1028d46bb6bbc524f63
Johnathan <jjjlaw@connect.ust.hk>
2022-07-04 17:34:25 +0800

    Cleanup

--- 2021/haskell/app/D16.hs
+++ 2021/haskell/app/D16.hs
@@ -7,9 +7,8 @@ import           Text.Printf                    ( printf )
 import           Utils                   hiding ( count )
 
 
-type Version = Int
-type TypeID = Int
-data Packet = PacketLit Version TypeID Int | PacketOp Version TypeID [Packet] deriving Show
+data Packet = Packet Int Int PacketObj deriving Show
+data PacketObj = Literal Int | Operands [Packet] deriving Show
 
 
 main :: IO ()
@@ -30,51 +29,42 @@ parse p s = case runParser (p <* many (char '0')) "" s of
   Left  err -> trace (errorBundlePretty err) undefined
 
 packet :: Parser Packet
-packet = fst <$> packetWithLen
-
-packetWithLen :: Parser (Packet, Int)
-packetWithLen = do
-  pos     <- getOffset
+packet = do
   version <- fromBinary <$> bits 3
   typeID  <- fromBinary <$> bits 3
   if typeID == 4
-    then -- Literal.
-         do
-      bs' <- collectWhile (bits 5) $ \(b : _) -> b == '1'
-      let bs = map (drop 1) bs'
-      pos2 <- getOffset
-      return (PacketLit version typeID $ fromBinary $ concat bs, pos2 - pos)
+    then do
+      bs <- collectWhile (bits 5) $ \(b : _) -> b == '1'
+      return $ Packet version typeID $ Literal $ fromBinary $ concatMap (drop 1) bs
     else do
-      lenTypeId <- fromBinary <$> bits 1
-      if lenTypeId == 0
-        then -- Length-bounded.
-             do
-          len      <- fromBinary <$> bits 15
-          children <- parse (some packet) <$> bits len
-          pos2     <- getOffset
-          return (PacketOp version typeID children, pos2 - pos)
-        else -- Multi.
-             do
-          num      <- fromBinary <$> bits 11
-          children <- count num packet
-          pos2     <- getOffset
-          return (PacketOp version typeID children, pos2 - pos)
+      lenTypeID <- fromBinary <$> bits 1
+      children <- operands lenTypeID
+      return $ Packet version typeID (Operands children)
  where
-  bits :: Int -> Parser String
-  bits n = count n digitChar -- Parses n digits (assume to be bits).
   collectWhile p f = do -- Parse while condition is true.
     x <- p
     if f x then (x :) <$> collectWhile p f else return [x]
 
+operands :: Int -> Parser [Packet]
+operands 0 = do
+  len      <- fromBinary <$> bits 15
+  parse (some packet) <$> bits len
+operands _ = do
+  num      <- fromBinary <$> bits 11
+  count num packet
+
+bits :: Int -> Parser String
+bits n = count n digitChar -- Parses n digits (assume to be bits).
 
 part1 :: Packet -> Int
-part1 (PacketLit v _ _ ) = v
-part1 (PacketOp  v _ ps) = v + sum (map part1 ps)
+part1 (Packet v _ obj) = case obj of
+  Literal _ -> v
+  Operands ps -> v + sum (map part1 ps)
 
 part2 :: Packet -> Int
-part2 (PacketLit _ _ x) = x
-part2 (PacketOp _ op ps) =
-  case op of
+part2 (Packet _ op obj) = case obj of
+  Literal x -> x
+  Operands ps -> case op of
       0 -> sum
       1 -> product
       2 -> minimum
