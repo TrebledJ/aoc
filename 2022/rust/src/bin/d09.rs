967041fb682a90cc7b1b048d31f27c43f9325725
Johnathan <jjjlaw@connect.ust.hk>
2022-12-15 20:04:42 +0800

    solve 2022/d09

--- 2022/rust/src/bin/d09.rs
+++ 2022/rust/src/bin/d09.rs
@@ -1,13 +1,12 @@
-use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs;
 use std::str::FromStr;
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 enum Dir {
     L,
-    R,
     U,
+    R,
     D,
 }
 
@@ -45,12 +44,6 @@ fn parse(contents: String) -> Vec<(Dir, u32)> {
 
 type Point = (i32, i32);
 
-// U --> tail will move either up-left, up, up-right, or not move at all.
-// up-left: occurs only if h is at offset (-1, -1).
-// up-right: occurs only if h is at offset (1, -1).
-// up: occurs only if h is at offset (0, -1).
-// At any other offset, the tail doesn't move.
-
 fn next((x, y): Point, dir: Dir) -> Point {
     match dir {
         Dir::L => (x - 1, y),
@@ -60,41 +53,46 @@ fn next((x, y): Point, dir: Dir) -> Point {
     }
 }
 
-fn step((hx, hy): Point, (tx, ty): Point, dir: Dir) -> Point {
-    let offsets: [Point; 3] = match dir {
-        Dir::L => [(-1, 1), (-1, 0), (-1, -1)],
-        Dir::R => [(1, -1), (1, 0), (1, 1)],
-        Dir::U => [(-1, -1), (0, -1), (1, -1)],
-        Dir::D => [(1, 1), (0, 1), (-1, 1)],
-    };
-
-    for (ox, oy) in offsets {
-        if (tx + ox, ty + oy) == (hx, hy) {
-            return (hx, hy); // Tail follows head.
-        }
+fn follow((hx, hy): Point, (tx, ty): Point) -> Point {
+    let xdiff = hx.abs_diff(tx);
+    let ydiff = hy.abs_diff(ty);
+    if xdiff > 1 || ydiff > 1 {
+        (tx + (hx - tx).signum(), ty + (hy - ty).signum())
+    } else {
+        (tx, ty)
     }
-    (tx, ty) // Tail doesn't move.
 }
 
 fn part1(xs: &Vec<(Dir, u32)>) -> u32 {
+    simulate(xs, 2)
+}
+
+fn part2(xs: &Vec<(Dir, u32)>) -> u32 {
+    simulate(xs, 10)
+}
+
+/**
+ * @brief   Simulates a rope with `joints` joints.
+ * @return  Number of unique locations visited by the tail.
+ */
+fn simulate(xs: &Vec<(Dir, u32)>, joints: usize) -> u32 {
     // 1. Expand directions (e.g. R 3 => R R R).
     // 2. Flatten the iterator.
     // 3. Fold over a mutable hashset, inserting the tail point on each fold.
     // 4. Return the length of the hashset, which is the number of visited points.
-
+    let init = vec![(0, 0); joints];
     let mut visited: HashSet<Point> = HashSet::new();
     xs.iter()
         .map(|(dir, count)| std::iter::repeat(dir).take(*count as usize))
         .flatten()
-        .fold((&mut visited, (0, 0), (0, 0)), |(visited, h, t), dir| {
-            let h2 = next(h, *dir);
-            let t2 = step(h, t, *dir);
-            visited.insert(t2);
-            (visited, h2, t2)
+        .fold(init, |v, dir| {
+            let mut v_next = Vec::new();
+            v_next.push(next(v[0], *dir));
+            v.iter().skip(1).for_each(|t| {
+                v_next.push(follow(*v_next.last().unwrap(), *t));
+            });
+            visited.insert(*v_next.last().unwrap());
+            v_next
         });
     visited.len() as u32
-}
-
-fn part2(xs: &Vec<(Dir, u32)>) -> u32 {
-    0
 }
